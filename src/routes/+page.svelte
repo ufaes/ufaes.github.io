92f71e77aa66508be2c992d2c303f2167b30802d
Herschenglime <herschenglime@gmail.com>
2025-06-29 23:00:57 -0700

    Added some more components for a believable homepage, need to fill text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,10 +9,8 @@
   <div class="hero-content text-neutral-content text-center">
     <div class="max-w-md">
       <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-      <p class="mb-5">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
+      <p class="mb-5">put some introductory copy for the club here.</p>
+      <p class="mb-5">furthermore, change the background to something cool and us-related: concert pics?</p>
       <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
